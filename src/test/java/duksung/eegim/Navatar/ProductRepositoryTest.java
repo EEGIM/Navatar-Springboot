2cfa42aa45f2417c898695cf19eb60eec49d8b56
oiatmil <oistmil@gmail.com>
2022-08-07 23:27:37 +0900

    0807 docker test 2

--- src/test/java/duksung/eegim/Navatar/ProductRepositoryTest.java
+++ src/test/java/duksung/eegim/Navatar/ProductRepositoryTest.java
@@ -1,25 +1,8 @@
 package duksung.eegim.Navatar;
 
-import duksung.eegim.Navatar.domain.Product.Product;
-import duksung.eegim.Navatar.domain.repository.ProductRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ProductRepositoryTest {
 
-    @Autowired
-    ProductRepository productRepository;
 
-    @Test
-    public void 상품_불러오기(){
-
-    }
 
 }
--- src/test/java/duksung/eegim/Navatar/UserApiControllerTest.java
+++ src/test/java/duksung/eegim/Navatar/UserApiControllerTest.java
@@ -1,38 +1,6 @@
 package duksung.eegim.Navatar;
 
-import duksung.eegim.Navatar.domain.User.User;
-import duksung.eegim.Navatar.domain.repository.UserRepository;
-import duksung.eegim.Navatar.web.dto.UserRegisterDto;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserApiControllerTest {
 
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Test
-    public void 회원등록() throws Exception {
-
-    }
 }
